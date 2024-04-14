04e673ced888bd9091ffa496f8b2a0e8afdfff5b
Anand <anandrnq.singh@gmail.com>
2024-04-14 07:06:28 +0530

    added routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import Genre from './pages/genre/Genre';
 import Registration from './pages/registration/Registration';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Registration/>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path = "/"
+            element={<Registration />}
+          ></Route>
+          <Route
+            path = "/Genre"
+            element={<Genre />}
+          ></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
