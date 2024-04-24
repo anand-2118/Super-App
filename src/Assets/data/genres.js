2b4dde517d93150ea8a18f942ae53e751730f487
Anand <anandrnq.singh@gmail.com>
2024-04-24 09:45:02 +0530

    commit

---
+++ src/Assets/data/genres.js
@@ -0,0 +1,48 @@
+import action from "../../Assets/action.png";
+import drama from "../../Assets/drama.png";
+import fantasy from "../../Assets/fantasy.png";
+import fiction from "../../Assets/fiction.png";
+import horror from "../../Assets/horror.png";
+import music from "../../Assets/music.png";
+import romance from "../../Assets/romance.png";
+import thriller from "../../Assets/thriller.png";
+import Western from "../../Assets/western.png";
+
+export const genres = [
+    {
+        title: "Action",
+        bgImage: action,
+    },
+    {
+        title: "Drama",
+        bgImage: drama,
+    },
+    {
+        title: "Romance",
+        bgImage: romance,
+    },
+    {
+        title: "Thriller",
+        bgImage: thriller,
+    },
+    {
+        title: "Western",
+        bgImage: Western,
+    },
+    {
+        title: "Horror",
+        bgImage: horror,
+    },
+    {
+        title: "Fantasy",
+        bgImage: fantasy,
+    },
+    {
+        title: "Music",
+        bgImage: music,
+    },
+    {
+        title: "Fiction",
+        bgImage: fiction,
+    },
+];
--- src/Pages/genre/Genre.js
+++ src/Pages/genre/Genre.js
@@ -1,9 +1,94 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import styles from "./Genre.module.css";
+import { genres } from "../../Assets/data/genres";
+import { color } from "../../Assets/data/color";
+import { useNavigate } from "react-router-dom";
 
-export default function Genre() {
-  return (
-    <div>
-        hello
-    </div>
-  )
+function GenrePage() {
+	const [selectedGenres, setSelectedGenres] = useState([]); 
+	const [lengthWarning, setLengthWarning] = useState(false);
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		if (selectedGenres.length >= 3) {
+			setLengthWarning(false);
+		}
+		localStorage.setItem("selectedGenres", JSON.stringify(selectedGenres));
+		console.log(localStorage.getItem("selectedGenres"));
+	}, [selectedGenres]);
+
+	const removeGenre = (index) => {
+		console.log(index); // 3
+		const newGenres = selectedGenres.filter((item) => item !== index); // 5 !== 3
+		setSelectedGenres(newGenres);
+	};
+
+	const selectGenre = (index) => {
+		if (selectedGenres.includes(index)) {
+			setSelectedGenres((prev) => prev.filter((item) => item !== index));
+		} else {
+			setSelectedGenres((prev) => [...prev, index]);
+		}
+	};
+
+	const handleNext = () => {
+		if (selectedGenres.length < 3) {
+			setLengthWarning(true);
+		} else {
+			setLengthWarning(false);
+			navigate("/");
+		}
+	};
+
+	return (
+		<div className={styles.page}>
+			<div className={styles.left}>
+				<div className={styles.headers}>
+					<h1 className={styles.leftHeader}>Super app</h1>
+					<h2 className={styles.leftSubHeader}>
+						Choose your <br /> entertainment <br /> category
+					</h2>
+				</div>
+
+				<div className={styles.selected}>
+					{selectedGenres.map((item, index) => (
+						<div key={item} className={styles.selectedGenre}>
+							{genres[item].title}
+							<button onClick={() => removeGenre(item)}>x</button>
+						</div>
+					))}
+				</div>
+				{lengthWarning && (
+					<div className={styles.warning}>
+						 <div> &nbsp;Minimum 3 category required</div>
+					</div>
+				)}
+			</div>
+			<div className={styles.right}>
+				<div className={styles.genreGrid}>
+					{genres.map((genre, index) => (
+						<div
+							key={index}
+							className={styles.genreCard}
+							onClick={() => selectGenre(index)}
+							style={{
+								backgroundColor: color[index],
+								outline: selectedGenres.includes(index)
+									? "4px solid green"
+									: "",
+							}}
+						>
+							<div className={styles.title}> {genre.title}</div>
+							<img src={genre.bgImage} alt="backgroundImage" />
+						</div>
+					))}
+				</div>
+				<button className={styles.button} onClick={handleNext}>
+					Next Page
+				</button>
+			</div>
+		</div>
+	);
 }
+
+export default GenrePage;
