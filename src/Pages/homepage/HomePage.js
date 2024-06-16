3ce25ee192e72f6d9faa7b71ad76f2c32bcccb1a
Anand <anandrnq.singh@gmail.com>
2024-06-17 01:30:51 +0530

    updated userwidgets and newsWidgets

--- src/Pages/homepage/HomePage.js
+++ src/Pages/homepage/HomePage.js
@@ -9,7 +9,7 @@ export default function HomePage() {
     const [user, setUser] = useState(JSON.parse(localStorage.getItem("currentUser")) || {});
     const [selectedGenres, setSelectedGenres] = useState(JSON.parse(localStorage.getItem("selectedGenres")) || []);
     const [weather, setWeather] = useState()
-    const [news,setNews] = useState();
+    const [news, setNews] = useState();
 
 
     const genres = [
@@ -37,11 +37,11 @@ export default function HomePage() {
         try {
             const { data, status } = await axios.get(
                 `https://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=delhi`
-                
+
             );
             if (status === 200) {
                 setWeather(data.current)
-                console.log(WEATHER_API_KEY)
+                //console.log(WEATHER_API_KEY)
             }
         }
         catch (error) {
@@ -51,30 +51,85 @@ export default function HomePage() {
     };
     const fetchNewsData = async () => {
         try {
-            const { data,status } = await axios.get(
-                 `https://newsapi.org/v2/top-headlines?country=us&apiKey=${NEWS_API_KEY}`
+            const { data, status } = await axios.get(
+                `https://newsapi.org/v2/top-headlines?country=us&apiKey=${NEWS_API_KEY}`
             );
-            
-                setNews(data.articles)
-               console.log(data)
-            
+
+            setNews(data.articles[0])
+            console.log(data)
+
         }
         catch (error) {
             console.error("Failed to fetch news data", error);
         }
     };
 
-    useEffect(() => {
-        if (weather) {
-            const { condition, humidity, temp_c, gust_kph, wind_kph } = weather;
+    const formatDate = (date) => {
+        if (date) {
+            const formattedDate = new Date(news.publishedAt).toLocaleDateString("en-US", {
+                year: "numeric",
+                month: "long",
+                day: "numeric",
 
-            console.log(condition.text, humidity, temp_c, gust_kph, wind_kph)
+            })
+
+            const formattedTime = new Date(news.publishedAt).toLocaleTimeString("en-US", {
+
+                hour: "numeric",
+                minute: "numeric",
+                hour12: true
+            })
+
+            return `${formattedDate} ${formattedTime}`;
+
+        }else{
+            const formattedDate = new Date().toLocaleDateString("en-US", {
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+
+            })
+
+            const formattedTime = new Date().toLocaleTimeString("en-US", {
+
+                hour: "numeric",
+                minute: "numeric",
+                hour12: true
+            })
+
+            return `${formattedDate} ${formattedTime}`;
         }
-        if(news){
-            console.log(news)
+    }
+
+    useEffect(() => {
+
+        if (weather) {
+           // const { condition, humidity, temp_c, gust_kph, wind_kph } = weather;
+
+           // console.log(condition.text, humidity, temp_c, gust_kph, wind_kph)
+           console.log(weather)
         }
-        
-    }, [weather,news])
+        if (news) {
+            //console.log(news)
+            console.log(news.publishedAt)
+            console.log(new Date(news.publishedAt).toLocaleDateString("en-US", {
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+
+            }));
+
+            console.log(new Date(news.publishedAt).toLocaleTimeString("en-US", {
+
+                hour: "numeric",
+                minute: "numeric",
+                hour12: true
+            }));
+
+
+        }
+
+    }, [weather, news])
 
 
     return (
@@ -88,44 +143,48 @@ export default function HomePage() {
                         <h1>{user.username}</h1>
                         {selectedGenres.length > 0 && (
                             <div className={styles.genreGrid}>
-                                {selectedGenres.map((item, index) => (
-                                    <div className={styles.pill} key={index}>
-                                        {genres[item].title}
-                                    </div>
-                                ))}
+                                {selectedGenres
+                                    .filter((genre, index) => index < 4)
+                                    .map((genre) => (
+                                        <div className={styles.pill}>
+                                            {genres[genre].title}
+                                        </div>
+                                    ))}
                             </div>
                         )}
                     </div>
                 </div>
                 <div className={styles.weatherWidget}>
-                {weather && (
-                    <div className={styles.weatherWidget}>
-                        <h3>Weather in Delhi</h3>
-                        <p>{weather.condition.text}</p>
-                        <p>{weather.temp_c}°C</p>
-                        <p>pressure: {weather.pressure_mb}</p>
-                        <p>{weather.gust_kph}</p>
-                        <p>{weather.wind_kph}</p>
-                    </div>
-                )}
+                    {weather && (
+                        <div className={styles.weatherWidget}>
+                            <div className={styles.header}>
+
+                                {formatDate()}
+                            </div>
+                            <div className={styles.body}>
+                                <img src="https://cdn.weatherapi.com/weather/64x64/day/122.png" alt="" />
+                                <p>{weather.condition.text}</p>
+                                <p> {weather.pressure_mb}</p>
+                                <p>{weather.temp_c}°C</p>
+                                <p>{weather.gust_kph}</p>
+                                <p>{weather.wind_kph}</p>
+                            </div>
+                        </div>
+                    )}
                 </div>
 
             </div>
             <div className={styles.right}>
-            {news && (
+                {news && (
                     <div className={styles.newsWidget}>
-                        <h3>Top News Headlines</h3>
-                        {news.map((article, index) => (
-                            <div key={index} className={styles.newsArticle}>
-                                <h4>{article.title}</h4>
-                                <p>By {article.author || "Unknown"}</p>
-                                <p>{article.description}</p>
-                                <a href={article.url} target="_blank" rel="noopener noreferrer">Read more</a>
-                            </div>
-                        ))}
+                        <img src={news.urlToImage} alt="" />
+                        <h1> {news.title}</h1>
+                        <p>{formatDate(news.publishedAt)}</p>
+                        <p>{news.description}</p>
+                        )
                     </div>
                 )}
-                
+
             </div>
         </div>
     );
